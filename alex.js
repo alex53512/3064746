a803a685dbb3930fbc902e71c4ef5fdcd6675e3a
yilber <alexpechene572@gmail.com>
2025-10-21 16:57:39 -0500

    ejer matematicos

---
+++ alex.js
@@ -0,0 +1,54 @@
+function fnGeo() {
+    let datoVariable ="alex";
+    datovariable = document.getElementById("txtDato").value;
+    console.log("Ingrese el dato", datovariable);
+}
+
+function fnCalcularalex (){
+    let Numero1 = 0;
+    let Numero2 = 0;
+
+    numero1 = document.querySelector("#txtNum1").value;
+    numero2 = document.querySelector(".txtNum2").value;
+
+    let resultadosuma = parseInt(numero1) + parseInt(numero2);
+     console.log("la suma de los datos:", numero1 +  numero2, "es:" ,resultadosuma);
+
+    console.log(`La suma de los datos: ${numero1} + ${numero2} es: ${resultadosuma}`);
+
+}
+
+function fnCalcularalex (){
+  
+    numero1 = document.querySelector("#txtNum1").value;
+    numero2 = document.querySelector(".txtNum2").value;
+
+    let resultadoresta = parseFloat(numero1) - parseFloat(numero2);
+
+
+    console.log(`La resta de los datos: ${numero1} - ${numero2} es: ${resultadoresta}`);
+
+}
+
+function fnCalcularalex (){
+  
+    numero1 = document.querySelector("#txtNum1").value;
+    numero2 = document.querySelector(".txtNum2").value;
+
+    let resultadomultiplicacion = parseFloat(numero1) * parseFloat(numero2);
+
+
+    console.log(`La multiplicacion de los datos: ${numero1} * ${numero2} es: ${resultadomultiplicacion}`);
+
+}
+
+function fnCalcularalex() {
+    let numero1 = document.querySelector("#txtNum1").value;
+    let numero2 = document.querySelector(".txtNum2").value;
+
+    let resultadodividir = parseFloat(numero1) / parseFloat(numero2);
+
+    console.log(`La división de los datos: ${numero1} / ${numero2} es: ${resultadodividir}`);
+}
+
+
